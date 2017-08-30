9902115d1c4c31494d651228b420fecd5d1e10ac
Arek <t8534@o2.pl>
2017-08-30 15:55:08 +0200

    [libgps_tm] Add ring buffer and uart irq draft.

---
+++ utils/ring_buff.h
@@ -0,0 +1,22 @@
+
+//todo add ifded
+
+
+
+//TODO: It should be optimized to use many RingBuffers, OOP.
+
+
+void RB_Init(void);
+
+void RB_DeInit(void);
+
+bool_t RB_GetByte(uint8_t* val);
+
+bool_t RB_PutByte(uint8_t val);
+
+bool_t RB_IsBufferEmpty(void);
+
+bool_t RB_IsBufferFull(void);
+
+void RB_ClearBuffer(void);
+
