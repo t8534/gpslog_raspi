3978a674f20986d78667b0508f58085bbc398af0
Arek <t8534@o2.pl>
2017-08-31 10:06:17 +0200

    [libgps_tm] Add test GPRMC message.

---
+++ tests/tm_test.c
@@ -0,0 +1,10 @@
+
+
+// U-blox-6-Receiver-Description-Including-Protocol-Specification.pdf
+//
+// Message Structure:
+// $GPRMC,hhmmss,status,latitude,N,longitude,E,spd,cog,ddmmyy,mv,mvE,mode*cs<CR><LF>
+//
+//todo: add <CR><LF>
+static const char *gprmc_1 = "$GPRMC,083559.00,A,4717.11437,N,00833.91522,E,0.004,77.52,091202,,,A*57"
+
